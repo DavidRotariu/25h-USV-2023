c488ce6a3ab7424fe95914333cb9d891c216535d
David Rotariu <davidrotariu139@gmail.com>
2023-11-24 00:34:04 +0200

    Update urls.py

---
+++ client/urls.py
@@ -0,0 +1,15 @@
+BRIDGE_IP = "192.168.0.10"
+
+BASE_URL = f"http://{BRIDGE_IP}"
+USER_NAME = "davids laptop"
+
+USER_ID = "BG1il2x9vf5naYB0Evhri-xcNXexD90BsMZ87ktF"
+
+LIGHT_ID_1 = "14"
+LIGHT_ID_2 = "13"
+LIGHT_ID_3 = "16"
+LIGHT_ID_4 = "12"
+LIGHT_ID_5 = "17"
+LIGHT_ID_6 = "15"
+
+ENDPOINT = f"{BASE_URL}/api/{USER_ID}/lights"
